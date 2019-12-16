fd0546a2fa21fec37025325ebebaf877f4ff5f8f
EiEiKhaingCU <eek9087@gmail.com>
2019-12-16 14:04:24 +0630

    add test

--- ClinicSystem/Controllers/HomeController.cs
+++ ClinicSystem/Controllers/HomeController.cs
@@ -42,5 +42,9 @@ namespace ClinicSystem.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult Test()
+        {
+            return View();
+        }
     }
 }
