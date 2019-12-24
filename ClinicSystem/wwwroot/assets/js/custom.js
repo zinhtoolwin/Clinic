29e822e46fc5f7d42534dacd6179c84356692f11
zinhtoolwin <zinhtoolwin1996@gmail.com>
2019-12-24 14:50:41 +0630

    template change

--- ClinicSystem/wwwroot/assets/js/custom.js
+++ ClinicSystem/wwwroot/assets/js/custom.js
@@ -1,81 +1,191 @@
-/**************************************
-    Template Name: Micrology
-    Created By: HTML.Design
-    https://html.design
-**************************************/
+/******************************************
+    Version: 1.0
+/****************************************** */
 
 (function($) {
     "use strict";
 
-/**************************************
-TOOLTIP
-**************************************/
-
-    $(function() {
-        $('[data-toggle="tooltip"]').tooltip()
-    })
-
-/**************************************
-CLICK SCROLLING
-**************************************/
-
-    $('a[href*="#"]')
-      .not('[href="#"]')
-      .not('[href="#0"]')
-      .click(function(event) {
-        if (
-          location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-          && 
-          location.hostname == this.hostname
-        ) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-          if (target.length) {
-            event.preventDefault();
-            $('html, body').animate({
-              scrollTop: target.offset().top
-            }, 1000, function() {
-              var $target = $(target);
-              $target.focus();
-              if ($target.is(":focus")) { // Checking if the target was focused
-                return false;
-              } else {
-                $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-                $target.focus(); // Set focus again
-              };
-            });
-          }
-        }
-    });
-
-/**************************************
-BACK TO TOP
-**************************************/
-    jQuery(window).scroll(function(){
-        if (jQuery(this).scrollTop() > 1) {
-            jQuery('.dmtop').css({bottom:"25px"});
-        } else {
-            jQuery('.dmtop').css({bottom:"-100px"});
-        }
-    });
-    jQuery('.dmtop').click(function(){
-        jQuery('html, body').animate({scrollTop: '0px'}, 800);
-        return false;
-    });
-
-/**************************************
-SLIDE WIDGETS
-**************************************/
-
-    $(document).ready(function() {
-        $('#nav-expander').on('click', function(e) {
-            e.preventDefault();
-            $('body').toggleClass('nav-expanded');
-        });
-        $('#nav-close').on('click', function(e) {
-            e.preventDefault();
-            $('body').removeClass('nav-expanded');
-        });
-    });
+    /* ==============================================
+    Fixed menu
+    =============================================== */
     
+	$(window).on('scroll', function () {
+		if ($(window).scrollTop() > 50) {
+			$('.header_style_01').addClass('fixed-menu');
+		} else {
+			$('.header_style_01').removeClass('fixed-menu');
+		}
+	});
+
+    /* ==============================================
+    Back top
+    =============================================== */
+    jQuery(window).scroll(function() {
+        if (jQuery(this).scrollTop() > 1) {
+            jQuery('.dmtop').css({
+                bottom: "75px"
+            });
+        } else {
+            jQuery('.dmtop').css({
+                bottom: "-100px"
+            });
+        }
+    });
+
+    /* ==============================================
+       Loader -->
+        =============================================== */
+
+    $(window).load(function() {
+        $("#preloader").on(500).fadeOut();
+        $(".preloader").on(600).fadeOut("slow");
+    });
+
+	/* ==============================================
+     Flip Bar -->
+     =============================================== */
+	 
+	$(document).ready(function() {
+		$(".map-btn").on('click',function() {
+			$(".map-box").slideToggle("slow");
+		});
+	});
+	
+	/* ==============================================
+		Scroll to top  
+	============================================== */
+		
+	if ($('#scroll-to-top').length) {
+		var scrollTrigger = 100, // px
+			backToTop = function () {
+				var scrollTop = $(window).scrollTop();
+				if (scrollTop > scrollTrigger) {
+					$('#scroll-to-top').addClass('show');
+				} else {
+					$('#scroll-to-top').removeClass('show');
+				}
+			};
+		backToTop();
+		$(window).on('scroll', function () {
+			backToTop();
+		});
+		$('#scroll-to-top').on('click', function (e) {
+			e.preventDefault();
+			$('html,body').animate({
+				scrollTop: 0
+			}, 700);
+		});
+	}
+	
+    /* ==============================================
+     Fun Facts -->
+     =============================================== */
+
+    function count($this) {
+        var current = parseInt($this.html(), 10);
+        current = current + 50; /* Where 50 is increment */
+        $this.html(++current);
+        if (current > $this.data('count')) {
+            $this.html($this.data('count'));
+        } else {
+            setTimeout(function() {
+                count($this)
+            }, 30);
+        }
+    }
+    $(".stat_count, .stat_count_download").each(function() {
+        $(this).data('count', parseInt($(this).html(), 10));
+        $(this).html('0');
+        count($(this));
+    });
+
+	/* ==============================================
+     Bootstrap Touch Slider -->
+     =============================================== */
+		$('#bootstrap-touch-slider').bsTouchSlider();
+    /* ==============================================
+     Tooltip -->
+     =============================================== */
+    $('[data-toggle="tooltip"]').tooltip()
+    $('[data-toggle="popover"]').popover()
+
+    /* ==============================================
+     Contact -->
+     =============================================== */
+    jQuery(document).ready(function() {
+        $('#contactform').submit(function() {
+            var action = $(this).attr('action');
+            $("#message").slideUp(750, function() {
+                $('#message').hide();
+                $('#submit')
+                    .after('<img src="images/ajax-loader.gif" class="loader" />')
+                    .attr('disabled', 'disabled');
+                $.post(action, {
+                        first_name: $('#first_name').val(),
+                        last_name: $('#last_name').val(),
+                        email: $('#email').val(),
+                        phone: $('#phone').val(),
+                        select_service: $('#select_service').val(),
+                        select_price: $('#select_price').val(),
+                        comments: $('#comments').val(),
+                        verify: $('#verify').val()
+                    },
+                    function(data) {
+                        document.getElementById('message').innerHTML = data;
+                        $('#message').slideDown('slow');
+                        $('#contactform img.loader').fadeOut('slow', function() {
+                            $(this).remove()
+                        });
+                        $('#submit').removeAttr('disabled');
+                        if (data.match('success') != null) $('#contactform').slideUp('slow');
+                    }
+                );
+            });
+            return false;
+        });
+    });
+	
+    /* ==============================================
+     Code Wrapper -->
+     =============================================== */
+
+    $('.code-wrapper').on("mousemove", function(e) {
+        var offsets = $(this).offset();
+        var fullWidth = $(this).width();
+        var mouseX = e.pageX - offsets.left;
+
+        if (mouseX < 0) {
+            mouseX = 0;
+        } else if (mouseX > fullWidth) {
+            mouseX = fullWidth
+        }
+
+        $(this).parent().find('.divider-bar').css({
+            left: mouseX,
+            transition: 'none'
+        });
+        $(this).find('.design-wrapper').css({
+            transform: 'translateX(' + (mouseX) + 'px)',
+            transition: 'none'
+        });
+        $(this).find('.design-image').css({
+            transform: 'translateX(' + (-1 * mouseX) + 'px)',
+            transition: 'none'
+        });
+    });
+    $('.divider-wrapper').on("mouseleave", function() {
+        $(this).parent().find('.divider-bar').css({
+            left: '50%',
+            transition: 'all .3s'
+        });
+        $(this).find('.design-wrapper').css({
+            transform: 'translateX(50%)',
+            transition: 'all .3s'
+        });
+        $(this).find('.design-image').css({
+            transform: 'translateX(-50%)',
+            transition: 'all .3s'
+        });
+    });
+
 })(jQuery);
